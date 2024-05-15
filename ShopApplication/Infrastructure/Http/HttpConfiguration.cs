6bf16e654a5ff02993d35b95591269aef1324cea
MintTree117 <135687103+MintTree117@users.noreply.github.com>
2024-05-15 17:50:33 -0400

    Renamed solution folder.

--- ShopApplication/Infrastructure/Http/HttpConfiguration.cs
+++ ShopApplication/Infrastructure/Http/HttpConfiguration.cs
@@ -5,9 +5,8 @@ namespace ShopApplication.Infrastructure.Http;
 
 internal static class HttpConfiguration
 {
-    internal static void ConfigureHttp( this WebAssemblyHostBuilder builder )
+    public static void ConfigureHttp( this WebAssemblyHostBuilder builder )
     {
-        builder.Services.AddScoped( sp => new HttpClient { BaseAddress = new Uri( builder.HostEnvironment.BaseAddress ) } );
         builder.Services.AddScoped<IHttpService, HttpService>();
     }
 }
