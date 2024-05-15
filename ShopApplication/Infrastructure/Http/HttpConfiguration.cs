4fef9102c159ac66e3cdcc5dc84ad999fe394ca8
MintTree117 <135687103+MintTree117@users.noreply.github.com>
2024-05-15 18:48:14 -0400

    Added cascading authorized views (app.razor), and altered some http configuration.

--- ShopApplication/Infrastructure/Http/HttpConfiguration.cs
+++ ShopApplication/Infrastructure/Http/HttpConfiguration.cs
@@ -7,6 +7,10 @@ internal static class HttpConfiguration
 {
     public static void ConfigureHttp( this WebAssemblyHostBuilder builder )
     {
+        builder.Services.AddScoped( sp => new HttpClient { BaseAddress = new Uri( GetBaseUrl( builder ) ) } );
         builder.Services.AddScoped<IHttpService, HttpService>();
     }
+
+    static string GetBaseUrl( WebAssemblyHostBuilder builder ) =>
+        builder.Configuration["BaseUrl"] ?? throw new Exception( "Failed to get base url from IConfiguration during startup." );
 }
