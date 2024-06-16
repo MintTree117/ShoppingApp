6a04ebe9a338a7835ee1aa0d59434502928d8eba
MintTree117 <135687103+MintTree117@users.noreply.github.com>
2024-06-16 13:34:49 -0400

    All account functionality teasted and styled except for view-orders, and generate and recover login.

--- Shop/Utilities/Consts.cs
+++ Shop/Utilities/Consts.cs
@@ -6,13 +6,12 @@ internal static class Consts
     // ----------------------------------------------------------------------------------------------------------------
     // BASE
     internal const string ApiBase = "https://localhost:7212/api";
-    
-    // ACCOUNT ADDRESSES
-    internal const string ApiGetAddresses = ApiBase + "/account/addresses/view";
-    internal const string ApiAddAddress = ApiBase + "/account/addresses/add";
-    internal const string ApiUpdateAddress = ApiBase + "/account/addresses/update";
-    internal const string ApiDeleteAddress = ApiBase + "/account/addresses/delete";
 
+    // ACCOUNT REGISTRATION
+    internal const string ApiRegister = ApiBase + "/account/register";
+    internal const string ApiConfirmEmail = ApiBase + "/account/register/confirmEmail";
+    internal const string ApiResendConfirm = ApiBase + "/account/register/resendConfirmEmail";
+    
     // AUTHENTICATION
     internal const string ApiLogin = ApiBase + "/authentication/login";
     internal const string ApiTwoFactor = ApiBase + "/authentication/2fa";
@@ -22,24 +21,24 @@ internal static class Consts
     internal const string ApiResetPassword = ApiBase + "/authentication/reset";
     internal const string ApiLogout = ApiBase + "/authentication/logout";
     
-    // DELETE ACCOUNT
-    internal const string ApiDeleteAccount = ApiBase + "/account/delete";
-    
     // ACCOUNT PROFILE
     internal const string ApiGetAccountProfile = ApiBase + "/account/profile/view";
     internal const string ApiUpdateAccountProfile = ApiBase + "/account/profile/update";
-    
-    // ACCOUNT REGISTRATION
-    internal const string ApiRegister = ApiBase + "/account/register";
-    internal const string ApiConfirmEmail = ApiBase + "/account/register/confirmEmail";
-    internal const string ApiResendConfirm = ApiBase + "/account/register/resendConfirmEmail";
 
     // ACCOUNT SECURITY
     internal const string ApiViewSecurity = ApiBase + "/account/security/view";
     internal const string ApiUpdatePassword = ApiBase + "/account/security/updatePassword";
     internal const string ApiDisable2Fa = ApiBase + "/account/security/disable2fa";
     internal const string ApiUpdate2Fa = ApiBase + "/account/security/update2fa";
-    
+
+    // ACCOUNT ADDRESSES
+    internal const string ApiGetAddresses = ApiBase + "/account/addresses/view";
+    internal const string ApiAddAddress = ApiBase + "/account/addresses/add";
+    internal const string ApiUpdateAddress = ApiBase + "/account/addresses/update";
+    internal const string ApiDeleteAddress = ApiBase + "/account/addresses/delete";
+
+    // DELETE ACCOUNT
+    internal const string ApiDeleteAccount = ApiBase + "/account/delete";
     
     // CATALOG
     internal const string ApiGetCategories = ApiBase + "/categories";
@@ -54,12 +53,12 @@ internal static class Consts
     // HOME
     internal const string PageHome = "/";
     // AUTHENTICATION
-    internal const string PageEmailConfirm = "/emailConfirm";
-    internal const string PageEmailConfirmResend = "/emailResend";
+    internal const string PageEmailConfirm = "/confirmEmail";
+    internal const string PageEmailConfirmResend = "/resendEmailConfirmation";
     internal const string PageLoginOrRegister = "/loginOrRegister";
     internal const string PageLogout = "/logout";
-    internal const string PageForgotPassword = "/passwordForgot";
-    internal const string PageResetPassword = "/passwordReset";
+    internal const string PageForgotPassword = "/forgotPassword";
+    internal const string PageResetPassword = "/resetPassword";
     internal const string PageSessionExpired = "/sessionExpired";
     // ACCOUNT
     internal const string PageAccountBase = "/account";
