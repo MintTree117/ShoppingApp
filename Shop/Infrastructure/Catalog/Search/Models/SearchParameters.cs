2496b93c3920cf755761900484bd86f3e4734524
MintTree117 <135687103+MintTree117@users.noreply.github.com>
2024-06-21 07:18:06 -0400

    Updated catalog search markup.

--- Shop/Infrastructure/Catalog/Search/Models/SearchParameters.cs
+++ Shop/Infrastructure/Catalog/Search/Models/SearchParameters.cs
@@ -10,7 +10,9 @@ public class SearchParameters(
     int? maxPrice,
     int page,
     int pageSize,
-    int sortBy )
+    int sortBy,
+    int? posX,
+    int? posY )
 {
     public Guid? CategoryId { get; set; } = categoryId;
     public HashSet<Guid>? BrandIds { get; set; } = brandIds;
@@ -22,7 +24,9 @@ public class SearchParameters(
     public int Page { get; set; } = page;
     public int PageSize { get; set; } = pageSize;
     public int SortBy { get; set; } = sortBy;
-    
+    public int? PosX { get; set; } = posX;
+    public int? PosY { get; set; } = posY;
+
     public static SearchParameters Empty() =>
-        new( null, null, null, null, null, null, null, 1, 5, 0 );
+        new( null, null, null, null, null, null, null, 1, 5, 0, null, null );
 }
